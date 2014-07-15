2c9f16785a80a047179ec52402417472bb4e6fab
Vincent Dumoulin <vi.dumoulin@gmail.com>
2014-07-14 20:47:54 -0400

    Commit utils.py

---
+++ utils.py
@@ -0,0 +1,67 @@
+__authors__ = "Vincent Dumoulin"
+__copyright__ = "Copyright 2014, Vincent Dumoulin"
+__credits__ = ["Vincent Dumoulin"]
+__license__ = "GPL v2"
+__maintainer__ = "Vincent Dumoulin"
+__email__ = "vincent.dumoulin@umontreal.ca"
+
+
+def check_is_valid_name_string(name_string, string_type="name string"):
+    """
+    Checks that `name_string` is a valid name string, and raises an error
+    otherwise.
+
+    A valid name string must be a non-empty string.
+    """
+    if type(name_string) is not str:
+        raise ValueError(string_type + " must be a string")
+    if len(name_string) == 0:
+        raise ValueError(string_type + " must not be empty")
+
+
+def check_is_valid_item_name(item_name):
+    """
+    Checks that `item_name` is a valid item name, and raises an error
+    otherwise. Validity conditions are defined in `check_is_valid_name_string`.
+    """
+    check_is_valid_name_string(name_string=item_name, string_type="item name")
+
+
+def check_is_valid_item_price(item_price):
+    """
+    Checks that `item_price` is a valid item price, and raises an error
+    otherwise.
+    
+    A valid item price must be a positive (>= 0) float or int.
+    """
+    if type(item_price) not in (float, int):
+        raise ValueError("item price must be either float or int")
+    if item_price < 0:
+        raise ValueError("item price must be positive")
+
+
+def check_is_valid_item_barcode(item_barcode):
+    """
+    Checks that `item_barcode` is a valid item barcode, and raises an error
+    otherwise. Validity conditions are defined in `check_is_valid_name_string`.
+    """
+    check_is_valid_name_string(name_string=item_barcode,
+                               string_type="item barcode")
+
+
+def check_is_valid_item_category(item_category):
+    """
+    Checks that `item_category` is a valid item category, and raises an error
+    otherwise. Validity conditions are defined in `check_is_valid_name_string`.
+    """
+    check_is_valid_name_string(name_string=item_category,
+                               string_type="item category")
+
+
+def check_is_valid_item_shortcut(item_shortcut):
+    """
+    Checks that `item_shortcut` is a valid item shortcut, and raises an error
+    otherwise. Validity conditions are defined in `check_is_valid_name_string`.
+    """
+    check_is_valid_name_string(name_string=item_shortcut,
+                               string_type="item shortcut")
